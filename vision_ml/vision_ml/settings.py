1805a4afa47a2cdce7eebf185dbd466c3477b9af
greenMetro <basicjava@gmail.com>
2019-06-30 02:33:39 +0900

    opencv_dface import

--- vision_ml/vision_ml/settings.py
+++ vision_ml/vision_ml/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_ROOT_URL = '.'
+MEDIA_ROOT_URL = 'C:\\Users\\I348920\\PycharmProjects\\web_ml_lesson\\vision_ml\\media'
