bbf246474b478d5f3f85c43c522dadd609a0aed5
greenMetro <basicjava@gmail.com>
2019-06-30 02:34:09 +0900

    admin page

--- vision_ml/web_ml/admin.py
+++ vision_ml/web_ml/admin.py
@@ -1,3 +1,8 @@
+from .models import ImageUploadModel
 from django.contrib import admin
 
-# Register your models here.
+class upload_image_Admin(admin.ModelAdmin):
+    list_display = ('description', 'document')
+
+
+admin.site.register(ImageUploadModel, upload_image_Admin)
