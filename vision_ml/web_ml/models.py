ceab6e1742fec2c4f987c7bae068a458d7644f1b
greenMetro <basicjava@gmail.com>
2019-07-09 10:53:30 +0900

    objList added

--- vision_ml/web_ml/models.py
+++ vision_ml/web_ml/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class ImageUploadModel(models.Model):
-    description = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=200, blank=True)
     document = models.ImageField(upload_to='images/%Y/%m/%d')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    objList = models.CharField(max_length=200, blank=True)
